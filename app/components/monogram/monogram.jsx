63ef1109ad52d398743d357849d9b590d8dde58e
jmailia <whatGravity@protonmail.com>
2024-05-27 08:53:13 -0700

    logo reduce thickness

--- app/components/monogram/monogram.jsx
+++ app/components/monogram/monogram.jsx
@@ -18,7 +18,7 @@ export const Monogram = forwardRef(({ highlight, className, ...props }, ref) => 
     >
       <defs>
         <clipPath id={clipId}>
-          <path d="M16 4L18.5 20L21 4H16ZM16 4L12 4M16 4L14 11L12 4M12 4L8 4M12 4L10 14L8 4M8 4L3 4L5.5 11L8 4Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+        <path d="M16 4L18.5 20L21 4H16ZM16 4L12 4M16 4L14 11L12 4M12 4L8 4M12 4L10 14L8 4M8 4L3 4L5.5 11L8 4Z" stroke="#000000" stroke-width="1.176" stroke-linecap="round" stroke-linejoin="round"></path>
         </clipPath>
       </defs>
       <rect clipPath={`url(#${clipId})`} width="100%" height="100%" />
