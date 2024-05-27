86c1130192107ae6a34cf6d0efa067554636ecee
jmailia <whatGravity@protonmail.com>
2024-05-27 10:45:26 -0700

    logo rotation change

--- app/components/monogram/monogram.jsx
+++ app/components/monogram/monogram.jsx
@@ -9,16 +9,18 @@ export const Monogram = forwardRef(({ highlight, className, ...props }, ref) => 
   return (
     <svg
       aria-hidden
-      className={classes(styles.monogram, className)}
+      //className={classes(styles.monogram, className)}
       width="140"
       height="100"
-      viewBox="0 0 20 30"
+      viewBox="-1.92 -1.92 27.84 27.84"
+      transform="rotate(55)matrix(1, 0, 0, 1, 0, 0)"
       ref={ref}
+      
       {...props}
     >
       <defs>
         <clipPath id={clipId}>
-        <path d="M16 4L18.5 20L21 4H16ZM16 4L12 4M16 4L14 11L12 4M12 4L8 4M12 4L10 14L8 4M8 4L3 4L5.5 11L8 4Z" stroke="#000000" stroke-width="1.176" stroke-linecap="round" stroke-linejoin="round"></path>
+        <path d="M16 4L18.5 20L21 4H16ZM16 4L12 4M16 4L14 11L12 4M12 4L8 4M12 4L10 14L8 4M8 4L3 4L5.5 11L8 4Z" ></path>
         </clipPath>
       </defs>
       <rect clipPath={`url(#${clipId})`} width="100%" height="100%" />
