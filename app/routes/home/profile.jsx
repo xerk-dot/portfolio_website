aae08fa5616349f861a18dc4b3c51d5ed84f2108
jmailia <whatGravity@protonmail.com>
2024-06-01 20:33:51 -0700

    henry addition

--- app/routes/home/profile.jsx
+++ app/routes/home/profile.jsx
@@ -1,6 +1,13 @@
 import profileImgLarge from '~/assets/images/pics_of_me/profile-large.jpg';
 import profileImgPlaceholder from '~/assets/images/pics_of_me/profile-placeholder.jpg';
 import profileImg from '~/assets/images/pics_of_me/profile.jpg';
+
+import hImg from 'app/assets/images/cyberpunk_ui/letters/h.png';
+import eImg from 'app/assets/images/cyberpunk_ui/letters/e.png';
+import nImg from 'app/assets/images/cyberpunk_ui/letters/n.png';
+import rImg from 'app/assets/images/cyberpunk_ui/letters/r.png';
+import yImg from 'app/assets/images/cyberpunk_ui/letters/y.png';
+
 //import { Button } from '~/components/button';
 import { DecoderText } from '~/components/decoder-text';
 import { Divider } from '~/components/divider';
@@ -12,7 +19,6 @@ import { Text } from '~/components/text';
 import { Transition } from '~/components/transition';
 import React, { Fragment, useState } from 'react';
 import { media } from '~/utils/style';
-import katakana from './katakana.svg';
 import styles from './profile.module.css';
 
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
@@ -118,16 +124,63 @@ export const Profile = ({ id, visible, sectionRef }) => {
                   delay={100}
                   placeholder={profileImgPlaceholder}
                   srcSet={`${profileImg} 480w, ${profileImgLarge} 960w`}
-                  width={960}
+                  width={1960}
                   height={1280}
                   sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
                   alt="Me smiling in an overexposed selfie."
                 />
-                <svg className={styles.svg} data-visible={visible} viewBox="0 0 136 766">
-                  <use href={`${katakana}#katakana-profile`} />
-                </svg>
+                <div className={styles.name}>
+                  <Image
+                      reveal
+                      delay={100}
+                      srcSet={`${hImg} 480w, ${hImg} 960w`}
+                      width={100}
+                      height={100}
+                      sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
+                      alt="h"
+                    />
+                    <Image
+                      reveal
+                      delay={100}
+                      srcSet={`${eImg} 480w, ${eImg} 960w`}
+                      width={100}
+                      height={100}
+                      sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
+                      alt="e"
+                    />
+                    <Image
+                      reveal
+                      delay={100}
+                      srcSet={`${nImg} 480w, ${nImg} 960w`}
+                      width={100}
+                      height={100}
+                      sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
+                      alt="n"
+                    />
+                    <Image
+                      reveal
+                      delay={100}
+                      srcSet={`${rImg} 480w, ${rImg} 960w`}
+                      width={100}
+                      height={100}
+                      sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
+                      alt="r"
+                    />
+                    <Image
+                      reveal
+                      delay={100}
+                      srcSet={`${yImg} 480w, ${yImg} 960w`}
+                      width={100}
+                      height={100}
+                      sizes={`(max-width: ${media.mobile}px) 100vw, 480px`}
+                      alt="y"
+                    />
+                </div>
               </div>
+
             </div>
+
+
           </div>
         )}
       </Transition>
